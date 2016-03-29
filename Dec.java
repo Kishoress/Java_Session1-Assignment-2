6c2acfedb2d8498199ad4db9e8ad3396b8a5732a
Kishoress <shrek.kishore@gmail.com>
2016-03-29 12:14:29 +0530

    Added files via upload

---
+++ Dec.java
@@ -0,0 +1,13 @@
+package Assign2;
+
+public class Dec {
+
+	public static void main(String[] args) {
+		
+   System.out.println("Decleration of 3 Numbers in Single Declaration ");
+   @SuppressWarnings("unused")
+   int a,b=4,c;
+   System.out.println("Value of B : "+ b);
+	}
+
+}
